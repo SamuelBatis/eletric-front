12d2b53a701ea9e30650391d764eff843791cb37
Samuel <samuel.batistela12@gmail.com>
2023-05-26 18:00:34 -0300

    feat: busca departamentos

--- pages/departamentos.js
+++ pages/departamentos.js
@@ -1,19 +1,165 @@
+import { useState, useEffect } from "react";
+import {
+  Box,
+  Button,
+  Heading,
+  Text,
+  VStack,
+  HStack,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  Input,
+  IconButton,
+} from "@chakra-ui/react";
+import { AddIcon, DeleteIcon } from "@chakra-ui/icons";
+import {
+  fetchDepartments,
+  createDepartment,
+} from "../services/api";
 import Header from "@/components/Header";
-import { Button, Card } from "@chakra-ui/react";
-import { Box, Heading, Text, VStack } from "@chakra-ui/react";
 
-const departments = [
+
+/*const departmentsData = [
   {
     name: "Departamento 1",
-    machines: ["Máquina A", "Máquina B", "Máquina C"],
+    machines: [
+      {
+        id: 1,
+        name: "Máquina A",
+        voltage: "220V",
+        usageTime: "8 horas",
+      },
+      {
+        id: 2,
+        name: "Máquina B",
+        voltage: "110V",
+        usageTime: "6 horas",
+      },
+    ],
   },
   {
     name: "Departamento 2",
-    machines: ["Máquina X", "Máquina Y", "Máquina Z"],
+    machines: [
+      {
+        id: 3,
+        name: "Máquina X",
+        voltage: "220V",
+        usageTime: "10 horas",
+      },
+      {
+        id: 4,
+        name: "Máquina Y",
+        voltage: "110V",
+        usageTime: "4 horas",
+      },
+    ],
   },
-];
+  // Adicione mais departamentos e máquinas conforme necessário
+];*/
 
 export default function Departamentos() {
+  const [isMachineModalOpen, setMachineModalOpen] = useState(false);
+  const [isDepartmentModalOpen, setDepartmentModalOpen] = useState(false);
+  const [newMachineName, setNewMachineName] = useState("");
+  const [newMachineVoltage, setNewMachineVoltage] = useState("");
+  const [newMachineUsageTime, setNewMachineUsageTime] = useState("");
+  const [newDepartmentName, setNewDepartmentName] = useState("");
+  const [departments, setDepartments] = useState([]);
+
+  useEffect(() => {
+    const getDepartments = async () => {
+      try {
+        const departmentsData = await fetchDepartments();
+        setDepartments(departmentsData);
+      } catch (error) {
+        // Handle error fetching departments
+      }
+    };
+    const getMachines = async () => {
+      try {
+        const machinesData = await fetchMachines();
+        setMachines(machinesData);
+      } catch (error) {
+        // Handle error fetching machines
+      }
+    };
+
+    //getMachines();
+
+    getDepartments();
+  }, []);
+
+
+
+
+  const handleAddMachine = async (departmentIndex) => {
+    const updatedDepartments = [...departments];
+    const newMachine = {
+      id: Date.now(),
+      name: newMachineName,
+      voltage: newMachineVoltage,
+      usageTime: newMachineUsageTime,
+    };
+
+    try {
+      await createMachine(newMachine);
+      updatedDepartments[departmentIndex].machines.push(newMachine);
+      setDepartments(updatedDepartments);
+      setMachineModalOpen(false);
+      setNewMachineName("");
+      setNewMachineVoltage("");
+      setNewMachineUsageTime("");
+    } catch (error) {
+      // Handle error creating machine
+    }
+  };
+
+
+  const handleDeleteMachine = async (departmentIndex, machineId) => {
+    try {
+      await api.delete(`/machine/${machineId}`);
+      const updatedDepartments = [...departments];
+      const machines = updatedDepartments[departmentIndex].machines;
+      const updatedMachines = machines.filter((machine) => machine.id !== machineId);
+      updatedDepartments[departmentIndex].machines = updatedMachines;
+      setDepartments(updatedDepartments);
+    } catch (error) {
+      // Handle error deleting machine
+    }
+  };
+
+
+  const handleAddDepartment = async () => {
+    const newDepartment = {
+      nome: newDepartmentName,
+    };
+
+    try {
+      await createDepartment(newDepartment);
+      setDepartments([...departments, newDepartment]);
+      setDepartmentModalOpen(false);
+      setNewDepartmentName("");
+      console.log("entrou")
+    } catch (error) {
+      // Handle error creating department
+    }
+  };
+  const calculateTotalCost = (voltage, usageTime) => {
+    // Suponha que o custo por minuto seja de R$ 0,10 para 110V e R$ 0,15 para 220V
+    const costPerMinute = voltage === "110V" ? 0.10 : 0.15;
+    const minutes = parseInt(usageTime.split(" ")[0]); // Extrai o valor numérico do tempo de uso (assumindo o formato "X horas")
+    const totalCost = costPerMinute * minutes;
+    return totalCost.toFixed(2); // Retorna o custo total formatado com duas casas decimais
+  };
+
+
   return (
     <>
       <Header />
@@ -22,22 +168,144 @@ export default function Departamentos() {
           Departamentos
         </Heading>
 
-        {departments.map((department, index) => (
-          <Box key={index} p={4} borderWidth={1} borderRadius="md" mb={4}>
+        {departments.map((department, departmentIndex) => (
+          <Box key={departmentIndex} p={4} borderWidth={1} borderRadius="md" mb={4}>
             <Heading as="h2" size="md" mb={2}>
-              {department.name}
+              {department.nome}
             </Heading>
             <Text>Máquinas:</Text>
-            <VStack align="start" spacing={2} mt={2}>
-              {department.machines.map((machine, i) => (
-                <Text key={i}>{machine}</Text>
-              ))}
-            </VStack>
-            <Button colorScheme="blue" size="xs"> Add Maquina </Button>
+            <HStack align="start" spacing={4} mt={2} flexWrap="wrap">
+              {/*department.machines.map((machine) => (
+              <Box
+                key={machine.id}
+                borderWidth={1}
+                borderRadius="md"
+                p={2}
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <VStack align="start" spacing={1}>
+                  <Text>
+                    <strong>Nome:</strong> {machine.name}
+                  </Text>
+                  <Text>
+                    <strong>Voltagem:</strong> {machine.voltage}
+                  </Text>
+                  <Text>
+                    <strong>Tempo de Uso:</strong> {machine.usageTime}
+                  </Text>
+                  <Text>
+                    <strong>Custo Total:</strong> {calculateTotalCost(machine.voltage, machine.usageTime)}
+                  </Text>
+                </VStack>
+                <IconButton
+                  icon={<DeleteIcon />}
+                  colorScheme="red"
+                  variant="ghost"
+                  onClick={() => handleDeleteMachine(departmentIndex, machine.id)}
+                />
+              </Box>
+            ))*/}
+            </HStack>
+            <Button
+              mt={4}
+              colorScheme="blue"
+              size="sm"
+              onClick={() => setMachineModalOpen(true)}
+            >
+              Adicionar Máquina
+            </Button>
           </Box>
         ))}
-      </Box>{" "}
-      <Button colorScheme="blue" ml="45%"> Add Departamento</Button>
+
+        <IconButton
+          icon={<AddIcon />}
+          colorScheme="blue"
+          size="lg"
+          position="fixed"
+          bottom={4}
+          right={4}
+          onClick={() => setDepartmentModalOpen(true)}
+        />
+
+        {/* Modal para adicionar máquinas */}
+        <Modal
+          isOpen={isMachineModalOpen}
+          onClose={() => setMachineModalOpen(false)}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Adicionar Máquina</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <FormControl>
+                <FormLabel>Nome da Máquina</FormLabel>
+                <Input
+                  value={newMachineName}
+                  onChange={(e) => setNewMachineName(e.target.value)}
+                />
+              </FormControl>
+              <FormControl mt={4}>
+                <FormLabel>Voltagem</FormLabel>
+                <Input
+                  value={newMachineVoltage}
+                  onChange={(e) => setNewMachineVoltage(e.target.value)}
+                />
+              </FormControl>
+              <FormControl mt={4}>
+                <FormLabel>Tempo de Uso</FormLabel>
+                <Input
+                  value={newMachineUsageTime}
+                  onChange={(e) => setNewMachineUsageTime(e.target.value)}
+                />
+              </FormControl>
+            </ModalBody>
+            <ModalFooter>
+              <Button
+                colorScheme="blue"
+                mr={3}
+                onClick={() => handleAddMachine(0)} // Passe o índice do departamento correto
+              >
+                Adicionar
+              </Button>
+              <Button onClick={() => setMachineModalOpen(false)}>Cancelar</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+
+        {/* Modal para adicionar departamentos */}
+        <Modal
+          isOpen={isDepartmentModalOpen}
+          onClose={() => setDepartmentModalOpen(false)}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Adicionar Departamento</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <FormControl>
+                <FormLabel>Nome do Departamento</FormLabel>
+                <Input
+                  value={newDepartmentName}
+                  onChange={(e) => setNewDepartmentName(e.target.value)}
+                />
+              </FormControl>
+            </ModalBody>
+            <ModalFooter>
+              <Button
+                colorScheme="blue"
+                mr={3}
+                onClick={handleAddDepartment}
+              >
+                Adicionar
+              </Button>
+              <Button onClick={() => setDepartmentModalOpen(false)}>Cancelar</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Box>
+
     </>
   );
 }
--- services/api.js
+++ services/api.js
@@ -6,7 +6,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
+    const token = sessionStorage.getItem("token");
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }
@@ -16,5 +16,54 @@ api.interceptors.request.use(
     Promise.reject(error);
   }
 );
+
+export const findUser = async (login) => {
+  try {
+    const response = await api.get(`/users/${login}`)
+    return response.data
+  } catch(error) {
+    console.error("Error find user", error)
+  }
+}
+
+export const fetchDepartments = async () => {
+  try {
+    const response = await api.get("/departamentos");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching departments:", error);
+    throw error;
+  }
+};
+
+export const createDepartment = async (department) => {
+  try {
+    const response = await api.post("/departamentos", department);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating department:", error);
+    throw error;
+  }
+};
+
+export const fetchMachines = async () => {
+  try {
+    const response = await api.get("/machine");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching machines:", error);
+    throw error;
+  }
+};
+
+export const createMachine = async (machine) => {
+  try {
+    const response = await api.post("/machine", machine);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating machine:", error);
+    throw error;
+  }
+};
 
 export default api;
